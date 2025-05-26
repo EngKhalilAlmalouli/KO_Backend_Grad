3fa6e1245d7e2e64289b80b021b054e32a9e2282
EngKhalilAlmalouli <engkhalilalmalouli@gmail.com>
2025-05-27 00:18:53 +0300

    add image table + edite security

--- src/main/java/com/example/ko_app/Security/User/Role.java
+++ src/main/java/com/example/ko_app/Security/User/Role.java
@@ -1,69 +1,69 @@
-//package com.example.ko_app.Security.User;
-//
-//import org.springframework.security.core.authority.SimpleGrantedAuthority;
-//
-//import java.util.Collections;
-//import java.util.List;
-//import java.util.Set;
-//import java.util.stream.Collectors;
-//
-//public enum Role {
-//
-//    USER(Collections.EMPTY_SET),
-//    ADMIN(
-//            Set.of(
-//                    Permission.ADMIN_READ,
-//                    Permission.ADMIN_UPDATE,
-//                    Permission.ADMIN_CREATE,
-//                    Permission.ADMIN_DELETE
-//            )
-//    ),
-//    SUBADMIN(
-//            Set.of(
-//                    Permission.SUBADMIN_READ,
-//                    Permission.SUBADMIN_UPDATE,
-//                    Permission.SUBADMIN_CREATE,
-//                    Permission.SUBADMIN_DELETE
-//
-//            )
-//    ),
-//
-//    CUSTOMER(
-//            Set.of(
-//            Permission.CUSTOMER_READ,
-//            Permission.CUSTOMER_UPDATE,
-//            Permission.CUSTOMER_CREATE,
-//            Permission.CUSTOMER_DELETE
-//
-//            )
-//    ),
-//
-//    DRIVER(
-//            Set.of(
-//                    Permission.DRIVER_READ,
-//                    Permission.DRIVER_UPDATE,
-//                    Permission.DRIVER_CREATE,
-//                    Permission.DRIVER_DELETE
-//
-//            )
-//    );
-//
-//    private final Set<Permission> permissionset;
-//
-//    Role(Set<Permission> permissionset) {
-//        this.permissionset = permissionset;
-//    }
-//
-//    public Set<Permission> getPermissionset() {
-//        return permissionset;
-//    }
-//
-//    public List<SimpleGrantedAuthority> getAuthority() {
-//        List<SimpleGrantedAuthority> auth = getPermissionset()
-//                .stream()
-//                .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
-//                .collect(Collectors.toList());
-//        auth.add(new SimpleGrantedAuthority("ROLE_" + this.name()));
-//        return auth;
-//    }
-//}
+package com.example.ko_app.Security.User;
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public enum Role {
+
+    USER(Collections.EMPTY_SET),
+    ADMIN(
+            Set.of(
+                    Permission.ADMIN_READ,
+                    Permission.ADMIN_UPDATE,
+                    Permission.ADMIN_CREATE,
+                    Permission.ADMIN_DELETE
+            )
+    ),
+    SUBADMIN(
+            Set.of(
+                    Permission.SUBADMIN_READ,
+                    Permission.SUBADMIN_UPDATE,
+                    Permission.SUBADMIN_CREATE,
+                    Permission.SUBADMIN_DELETE
+
+            )
+    ),
+
+    CUSTOMER(
+            Set.of(
+            Permission.CUSTOMER_READ,
+            Permission.CUSTOMER_UPDATE,
+            Permission.CUSTOMER_CREATE,
+            Permission.CUSTOMER_DELETE
+
+            )
+    ),
+
+    DRIVER(
+            Set.of(
+                    Permission.DRIVER_READ,
+                    Permission.DRIVER_UPDATE,
+                    Permission.DRIVER_CREATE,
+                    Permission.DRIVER_DELETE
+
+            )
+    );
+
+    private final Set<Permission> permissionset;
+
+    Role(Set<Permission> permissionset) {
+        this.permissionset = permissionset;
+    }
+
+    public Set<Permission> getPermissionset() {
+        return permissionset;
+    }
+
+    public List<SimpleGrantedAuthority> getAuthority() {
+        List<SimpleGrantedAuthority> auth = getPermissionset()
+                .stream()
+                .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
+                .collect(Collectors.toList());
+        auth.add(new SimpleGrantedAuthority("ROLE_" + this.name()));
+        return auth;
+    }
+}
--- src/main/java/com/example/ko_app/Security/User/User.java
+++ src/main/java/com/example/ko_app/Security/User/User.java
@@ -1,197 +1,197 @@
-//package com.example.ko_app.Security.User;
-//
-//import jakarta.persistence.*;
-//import org.springframework.security.core.GrantedAuthority;
-//import org.springframework.security.core.userdetails.UserDetails;
-//
-////import javax.management.relation.Role;
-//import com.example.ko_app.Security.User.Role;
-//
-//import java.util.Collection;
-//import java.util.List;
-//
-//@Entity
-//@Table(name = "_user")
-//public class User implements UserDetails {
-//
-//    @Id
-//    @SequenceGenerator(
-//            name = "user_id",
-//            sequenceName = "user_id",
-//            allocationSize = 1
-//    )
-//    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_id")
-//    private Integer id;
-//    private String firstname;
-//    private String lastname;
-//
-//    @Column(unique = true)
-//    private String email;
-//
-//    private String password;
-//
-//    @Enumerated(EnumType.STRING)
-//    private Role role;
-//
-//    //constructor
-//    public User() {
-//    }
-//
-//    public User(Integer id, String firstname, String lastname, String email, String password, Role role) {
-//        this.id = id;
-//        this.firstname = firstname;
-//        this.lastname = lastname;
-//        this.email = email;
-//        this.password = password;
-//        this.role = role;
-//    }
-//
-//    //Get Set
-//
-//
-//    public Integer getId() {
-//        return id;
-//    }
-//
-//    public void setId(Integer id) {
-//        this.id = id;
-//    }
-//
-//    public String getFirstname() {
-//        return firstname;
-//    }
-//
-//    public void setFirstname(String firstname) {
-//        this.firstname = firstname;
-//    }
-//
-//    public String getLastname() {
-//        return lastname;
-//    }
-//
-//    public void setLastname(String lastname) {
-//        this.lastname = lastname;
-//    }
-//
-//    public String getEmail() {
-//        return email;
-//    }
-//
-//    public void setEmail(String email) {
-//        this.email = email;
-//    }
-//
-//    public void setPassword(String password) {
-//        this.password = password;
-//    }
-//
-//    public Role getRole() {
-//        return role;
-//    }
-//
-//    public void setRole(Role role) {
-//        this.role = role;
-//    }
-//
-//    // toString
-//
-//
-//    @Override
-//    public String toString() {
-//        return "User{" +
-//                "id=" + id +
-//                ", firstname='" + firstname + '\'' +
-//                ", lastname='" + lastname + '\'' +
-//                ", email='" + email + '\'' +
-//                ", password='" + password + '\'' +
-//                ", role=" + role +
-//                '}';
-//    }
-//
-//    // ==== UserDetails Implementation ====
-//
-//    @Override
-//    public Collection<? extends GrantedAuthority> getAuthorities() {
-//        return List.of();
-//    }
-//
-//    @Override
-//    public String getPassword() {
-//        return password;
-//    }
-//
-//    @Override
-//    public String getUsername() {
-//        return email;
-//    }
-//
-//    @Override
-//    public boolean isAccountNonExpired() {
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean isAccountNonLocked() {
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean isCredentialsNonExpired() {
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean isEnabled() {
-//        return true;
-//    }
-//
-//    // Builder
-//
-//    public static class Builder {
-//        private Integer id;
-//        private String firstname;
-//        private String lastname;
-//        private String email;
-//        private String password;
-//        private Role role;
-//
-//        public Builder id(Integer id) {
-//            this.id = id;
-//            return this;
-//        }
-//
-//        public Builder firstname(String firstname) {
-//            this.firstname = firstname;
-//            return this;
-//        }
-//
-//        public Builder lastname(String lastname) {
-//            this.lastname = lastname;
-//            return this;
-//        }
-//
-//        public Builder email(String email) {
-//            this.email = email;
-//            return this;
-//        }
-//
-//        public Builder password(String password) {
-//            this.password = password;
-//            return this;
-//        }
-//
-//        public Builder role(Role role) {
-//            this.role = role;
-//            return this;
-//        }
-//
-//        public User build() {
-//            return new User(id, firstname, lastname, email, password, role);
-//        }
-//    }
-//
-//    public static Builder builder() {
-//        return new Builder();
-//    }
-//}
-//
+package com.example.ko_app.Security.User;
+
+import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+//import javax.management.relation.Role;
+import com.example.ko_app.Security.User.Role;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Table(name = "_user")
+public class User implements UserDetails {
+
+    @Id
+    @SequenceGenerator(
+            name = "user_id",
+            sequenceName = "user_id",
+            allocationSize = 1
+    )
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_id")
+    private Integer id;
+    private String firstname;
+    private String lastname;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    //constructor
+    public User() {
+    }
+
+    public User(Integer id, String firstname, String lastname, String email, String password, Role role) {
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    //Get Set
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    // toString
+
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", role=" + role +
+                '}';
+    }
+
+    // ==== UserDetails Implementation ====
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    // Builder
+
+    public static class Builder {
+        private Integer id;
+        private String firstname;
+        private String lastname;
+        private String email;
+        private String password;
+        private Role role;
+
+        public Builder id(Integer id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder firstname(String firstname) {
+            this.firstname = firstname;
+            return this;
+        }
+
+        public Builder lastname(String lastname) {
+            this.lastname = lastname;
+            return this;
+        }
+
+        public Builder email(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public Builder password(String password) {
+            this.password = password;
+            return this;
+        }
+
+        public Builder role(Role role) {
+            this.role = role;
+            return this;
+        }
+
+        public User build() {
+            return new User(id, firstname, lastname, email, password, role);
+        }
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+}
+
